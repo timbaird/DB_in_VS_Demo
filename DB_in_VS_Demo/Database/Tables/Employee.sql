aee4603956f6f423a9f41269f4d4b7fe35482ea0
tbaird <tbaird@swin.edu.au>
2021-08-30 09:37:55 +1000

    Tables Created

---
+++ DB_in_VS_Demo/Database/Tables/Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[StaffID] INT, 
+    [GivenName] NVARCHAR(100) NOT NULL, 
+    [Surname] NVARCHAR(100) NOT NULL, 
+    CONSTRAINT [PK_Employee] PRIMARY KEY (StaffID)
+)
---
+++ DB_in_VS_Demo/Database/Tables/Office.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Office]
+(
+	[LocCode] INT NOT NULL, 
+    [Address] NVARCHAR(200) NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_Office] PRIMARY KEY ([LocCode])
+)
